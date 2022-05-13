9253d7831a5b788d33cd6bc8a04660600e341f4e
Nathan Brown <nathan@nkbrown.us>
2022-05-13 16:04:18 -0700

    A little refactoring

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,27 +1,15 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-
-type Post = {
-  slug: string;
-  title: string;
-};
+import { getPosts } from "~/models/post.server";
 
 type LoaderData = {
-  posts: Array<Post>;
+  // this is a handy way to say: "posts is whatever type getPosts resolves to"
+  posts: Awaited<ReturnType<typeof getPosts>>;
 };
 
 export const loader = async () => {
   return json<LoaderData>({
-    posts: [
-      {
-        slug: "my-first-post",
-        title: "My First Post",
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I Made Just For You",
-      },
-    ],
+    posts: await getPosts(),
   });
 };
 
